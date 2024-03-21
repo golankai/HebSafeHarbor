4bedf66a465eab66915325414bbc8282cb8ee67b
Kai Golan <72255040+golankai@users.noreply.github.com>
2024-03-21 11:09:57 +0200

    Add files via upload

--- hebsafeharbor/manager.py
+++ hebsafeharbor/manager.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, Optional, List
 
 from hebsafeharbor import Doc
 from hebsafeharbor.anonymizer.phi_anonymizer import PhiAnonymizer
@@ -10,32 +10,48 @@ class HebSafeHarbor:
     anonymization process and return an anonymized text.
     """
 
-    def __init__(self):
+    def __init__(self, allow_list: Optional[List[str]] = None) -> None:
         """
         Initializes HebSafeHarbor
+        :param allow_list: List of words that the user defines as being allowed to keep
         """
         self.identifier = PhiIdentifier()
         self.anonymizer = PhiAnonymizer()
+        self.allow_list = allow_list
 
-    def __call__(self, doc_list: List[Dict[str, str]]) -> List[Doc]:
+    def __call__(self, doc_list: List[Dict[str, str]], allow_list: Optional[List[str]] = None, extend_base_allow_list: bool = False) -> List[Doc]:
         """
         The main method, executes the PHI reduction process on the given text
         :param doc_list: List of dictionary where each dict represents a document.
                         Each dictionary should consist of "id" and "text" columns
+        :param allow_list: List of words that the user defines as being allowed to keep
+        :param extend_base_allow_list: If True, the allow_list will be added to the base allow list (for this instance only),
+                        otherwise it will replace the base allow list
         :return: anonymized text
         """
+
         docs = [Doc(doc_dict) for doc_dict in doc_list]
-        docs = self.identify(docs)
+        docs = self.identify(docs, allow_list=allow_list, extend_base_allow_list=extend_base_allow_list)
         docs = self.anonymize(docs)
         return docs
 
-    def identify(self, docs: List[Doc]) -> List[Doc]:
+    def identify(self, docs: List[Doc], allow_list: Optional[List[str]] = None, extend_base_allow_list: bool = False) -> List[Doc]:
         """
         This method identifies the PHI entities in the input text
         :param docs: a list of Doc objects which contains the input text for anonymization
+        :param allow_list: List of words that the user defines as being allowed to keep
+        :param extend_base_allow_list: If True, the allow_list will be added to the base allow list (for this instance only),
+                        otherwise it will replace the base allow list
         :return: a list of the updated Doc objects that contains the recognized PHI entities
         """
-        return [self.identifier(doc) for doc in docs]
+
+        if allow_list:
+            if extend_base_allow_list:
+                allow_list.extend(self.allow_list)
+            # If allow_list is not None, and extend_base_allow_list is False, we use the given allow_list
+        else:
+            allow_list = self.allow_list
+        return [self.identifier(doc, allow_list) for doc in docs]
 
     def anonymize(self, docs: List[Doc]) -> List[Doc]:
         """
